135ee2457a528cfdead91a918f5e9eb4a201c643
r.verjus <romuald.verjus@viveris.fr>
2019-07-02 14:44:02 +0200

    add basic endpoints in the api

--- app.py
+++ app.py
@@ -2,10 +2,46 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+    {
+        'name': "my shop",
+        'items': [
+            {
+                'name': 'my item',
+                'price': 15.99
+            }
+        ]
+    }
+]
 
-@app.route('/')
-def home():
-    return "Hello world"
+
+# @app.route('/')
+# def home():
+#     return "Hello world"
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+@app.route('/store/<string:name>')
+def get_store():
+    pass
+
+
+@app.route('/store')
+def get_stores():
+    pass
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store():
+    pass
+
+
+@app.route('/store/<string:name>/item')
+def get_items_in_store():
+    pass
 
 
 app.run(port=5000)
